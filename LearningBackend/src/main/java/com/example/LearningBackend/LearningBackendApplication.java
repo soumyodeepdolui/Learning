30e53e109bfcc1857e2ace3e28b401a052507357
Soumyodeep Dolui <doluisoumyodeep@gmail.com>
2025-04-29 19:04:23 +0530

    SpringBoot Project Added

---
+++ LearningBackend/src/main/java/com/example/LearningBackend/LearningBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearningBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningBackendApplication.class, args);
+	}
+
+}
---
+++ LearningBackend/src/test/java/com/example/LearningBackend/LearningBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearningBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
